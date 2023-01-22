5345db994090a3e9519f932860db0ec5eb978bc3
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-01-21 17:10:07 -0800

    added css and javascript

---
+++ ask.js
@@ -0,0 +1,7 @@
+let ask = prompt (" Do you want to save the Lions?");
+if (ask == "yes") {
+    message= " Fanstastic thanks for your support!!";
+} else {
+    message = " Only 1$ a month can go to saving the Lions! ";
+}
+document.write(message);
---
+++ name.js
@@ -0,0 +1,3 @@
+let name = prompt("What is your name?");
+document.write("Hello "  + name);
+
